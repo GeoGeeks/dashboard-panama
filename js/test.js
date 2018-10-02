8e62e594826c6fdf0543f9f26fadf25eebefd7df
Marcelo Villa <marcelo.villa.p@gmail.com>
2018-10-02 15:00:27 -0500

    implementacion API Windy y prueba terremotos USGS

--- js/test.js
+++ js/test.js
@@ -1,35 +1,63 @@
-var map = L.map('map').setView([8.918, -79.409], 8);
-var layer = L.esri.basemapLayer('Topographic').addTo(map);
-var layerLabels;
+// define options
+const options = {
 
-function setBasemap(basemap) {
-    if (layer) {
-        map.removeLayer(layer);
-    }
+    // Required: API key
+    key: 'Hda2XFeROn7NWhHDEDWyoJqAdlTeiGO4',
 
-    layer = L.esri.basemapLayer(basemap);
+    // Optional: Initial state of the map
+    lat: 8.918,
+    lon: -79.409,
+    zoom: 8,
+};
 
-    map.addLayer(layer);
+// Initialize Windy API
+windyInit( options, windyAPI => {
+    // windyAPI is ready, and contain 'map', 'store',
+    // 'picker' and other usefull stuff
 
-    if (layerLabels) {
-        map.removeLayer(layerLabels);
-    }
+    const  { map }  = windyAPI;
+    // .map is instance of Leaflet map
 
-    if (basemap === 'ShadedRelief'
-        || basemap === 'Oceans'
-        || basemap === 'Gray'
-        || basemap === 'DarkGray'
-        || basemap === 'Terrain'
-    ) {
-        layerLabels = L.esri.basemapLayer(basemap + 'Labels');
-        map.addLayer(layerLabels);
-    } else if (basemap.includes('Imagery')) {
-        layerLabels = L.esri.basemapLayer('ImageryLabels');
-        map.addLayer(layerLabels);
-    }
-}
 
-function changeBasemap(basemaps){
-    var basemap = basemaps.value;
-    setBasemap(basemap);
-}
+    var DataFrame = dfjs.DataFrame;
+
+// bbox
+    var left = -82.9657830472 - 2,
+        bottom = 7.2205414901 - 2,
+        right = -77.2425664944 + 2,
+        top = 9.61161001224 + 2;
+
+    var test;
+
+    DataFrame.fromCSV('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv')
+        .then(df => {
+            console.log(df);
+            var df_pa;
+
+            test = df;
+
+            df_pa = df.filter(row => row.get('longitude') >= left && row.get('longitude') <= right);
+            df_pa = df_pa.filter(row => row.get('latitude') >= bottom && row.get('latitude') <= top);
+            console.log(df_pa);
+
+            df_pa.map(row => {
+
+                // obtener coordenadas (longitud y latitud)
+                const x = row.get('longitude');
+                const y = row.get('latitude');
+
+                marker = L.marker([y, x])
+                    .addTo(map);
+
+                var popUpTemplate = `
+                    Hello, World!
+                `;
+
+                marker.bindPopup(popUpTemplate);
+
+            });
+        });
+
+
+});
+
