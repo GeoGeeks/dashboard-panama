680b813924d004a05e75b1f2efb3764c5778d866
Marcelo Villa <marcelo.villa.p@gmail.com>
2018-10-04 12:49:46 -0500

    Agregar terremotos con popup

--- js/test.js
+++ js/test.js
@@ -9,7 +9,7 @@ const options = {
     // Optional: Initial state of the map
     lat: 8.918,
     lon: -79.409,
-    zoom: 8,
+    zoom: 6,
 };
 
 // Initialize Windy API
@@ -28,33 +28,39 @@ windyInit( options, windyAPI => {
         right = -77.2425664944 + 2,
         top = 9.61161001224 + 2;
 
-    function renderEarthquakes () {
-        DataFrame.fromCSV('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv')
-            .then(df => {
-                console.log(df);
-                var df_pa;
+    var prueba;
 
-                df_pa = df.filter(row => row.get('longitude') >= left && row.get('longitude') <= right);
-                df_pa = df_pa.filter(row => row.get('latitude') >= bottom && row.get('latitude') <= top);
+    DataFrame.fromCSV('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv')
+        .then(df => {
+            console.log(df);
+            var df_pa;
 
-                df_pa.map(row => {
 
-                    // obtener coordenadas (longitud y latitud)
-                    const x = row.get('longitude');
-                    const y = row.get('latitude');
 
-                    marker = L.marker([y, x])
-                        .addTo(map);
+            df_pa = df.filter(row => row.get('longitude') >= left && row.get('longitude') <= right);
+            df_pa = df_pa.filter(row => row.get('latitude') >= bottom && row.get('latitude') <= top);
 
-                    var popUpTemplate = `
-                    Hello, World!
+            df_pa.map(row => {
+
+                // obtener coordenadas (longitud y latitud)
+                const x = row.get('longitude');
+                const y = row.get('latitude');
+
+                marker = L.marker([y, x])
+                    .addTo(map);
+
+                var popUpTemplate = `
+                    <div class="earthquake-popup">
+                        <span class="bold">Magnitud</span>:   ${row.get('mag')}   <br>
+                        <span class="bold">Fecha</span>:      ${row.get('time')}  <br>
+                        <span class="bold">Lugar</span>:      ${row.get('place')} <br>
+                    </div>
                 `;
 
-                    marker.bindPopup(popUpTemplate);
-                });
+                marker.bindPopup(popUpTemplate);
+                prueba = df;
             });
-    }
-
+        });
 
 
 
